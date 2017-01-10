6f52028aaca5bff21900c251d3ebeea2d98a81c1
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2017-01-10 13:35:47 +0800

    pass params from js fn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-var addon = require('bindings')('xfyun');
+var xfyun = require('bindings')('xfyun');
 
-addon(function(msg){
-  console.log(msg); // 'hello world'
-});
+xfyun.iat("u", // username
+  "p", // password
+  "appid = 5864ae2d, work_dir = .", // login params
+  "sub = iat, domain = iat, language = zh_cn, accent = mandarin, sample_rate = 16000, result_type = plain, result_encoding = utf8",
+  "wav/iflytek01.wav", // audio file
+  function (result) {
+    console.log(result)
+  })
+
+
+// xfyun(function(userName, password, loginParams, ){
+//   console.log(msg); // 'hello world'
+// });
